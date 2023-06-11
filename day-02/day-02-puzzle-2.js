fce45c8f3e9203120b76bbb38973399afc3b4ad5
André Silva <dev01766@proton.me>
2023-06-11 13:55:38 +0200

    Create files day 02

---
+++ day-02/day-02-puzzle-2.js
@@ -0,0 +1,23 @@
+const readFileContent = require("../read-file-content");
+
+// Convert command-units strings into tuples with units (string) converter to number
+// [command, units]
+const commandReadings = readFileContent('day-02-input.txt')
+  .split('\n')
+  .map((reading) => reading
+    .split(' '))
+    .map((readingTuple) => [readingTuple[0], Number(readingTuple[1])])
+
+let aim = 0
+let horizontalPos = 0
+let depth = 0
+for (let commandAndUnits of commandReadings) {
+  if (commandAndUnits[0] === 'forward') {
+    horizontalPos += commandAndUnits[1]
+    depth += aim * commandAndUnits[1]
+  }
+  if (commandAndUnits[0] === 'up') aim -= commandAndUnits[1]
+  if (commandAndUnits[0] === 'down') aim += commandAndUnits[1]
+}
+
+console.log(horizontalPos * depth)
